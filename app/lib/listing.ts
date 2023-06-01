8c6b96db9b4cda403bb343525118295330344428
Asil <asilefeakyol@gmail.com>
2023-06-01 22:17:57 +0300

    listing functions

---
+++ app/lib/listing.ts
@@ -0,0 +1,41 @@
+import { db } from "~/utils/db.server";
+
+interface IGetListing {
+  request: any;
+  id: string;
+}
+
+export async function getListings() {
+  const querySnapshot = await db.collection("listings").get();
+
+  const data: any[] = [];
+  querySnapshot.forEach((doc: any) => {
+    data.push({ ...doc.data(), id: doc.id });
+  });
+
+  return data;
+}
+
+export async function getListing({ request, id }: IGetListing) {
+  const docSnapshot = await db.collection("listings").doc(id).get();
+
+  if (!docSnapshot.exists) {
+    throw Error("No such document exists");
+  } else {
+    const listing = docSnapshot.data();
+    return listing;
+  }
+}
+
+export async function createListing({ request, listing }: any) {
+  const { title, slug, description, price } = listing;
+
+  const res = await db.collection("listings").add({
+    title,
+    slug,
+    description,
+    price,
+  });
+
+  return getListing({ request, id: res.id });
+}
