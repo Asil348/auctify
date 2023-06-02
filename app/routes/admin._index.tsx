e1fc0189bc348347ed6159f641e4b9ebddcebd40
Asil <asilefeakyol@gmail.com>
2023-06-03 00:43:02 +0300

    comment

--- app/routes/admin._index.tsx
+++ app/routes/admin._index.tsx
@@ -5,6 +5,11 @@ import type { LoaderArgs } from "@remix-run/node";
 import { isUserAdmin } from "~/server/auth.server";
 
 export async function loader({ request, params }: LoaderArgs) {
+
+  // !! this is a bad way to check if a user is an admin !! //
+  // because we check it on every other admin screen
+  // instead, we should check it once when the user signs in
+  // and then store it in the session(?) or something
   const isAdmin = await isUserAdmin();
 
   if (!isAdmin) {
