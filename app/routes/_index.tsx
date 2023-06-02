e5eb0e70bc2bf231fdd543e446efdfe1f89821b4
Asil <asilefeakyol@gmail.com>
2023-06-02 19:37:26 +0300

    protect pages

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,9 @@
 // import type { V2_MetaFunction } from "@remix-run/node";
 
-import { Link } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
+import type { LoaderArgs } from "@remix-run/node";
+import { getUser, isUserAdmin } from "~/server/auth.server";
+import { checkSessionValidity } from "~/server/session.server";
 
 // export const meta: V2_MetaFunction = () => {
 //   return [
@@ -9,19 +12,42 @@ import { Link } from "@remix-run/react";
 //   ];
 // };
 
+export async function loader({ request }: LoaderArgs) {
+  const user = await getUser();
+
+  const isAdmin = await isUserAdmin();
+
+  checkSessionValidity(request);
+
+  return { user, isAdmin };
+}
+
 export default function Index() {
+  const { user, isAdmin } = useLoaderData();
+
   return (
     <div>
       <h1>Welcome to Auctify</h1>
       <Link to="/listings">Listings</Link>
       <br />
-      <Link to="/admin">Admin</Link>
-      <br />
-      <Link to="/signin">Sign in</Link>
-      <br />
-      <Link to="/signout">Sign out</Link>
-      <br />
-      <Link to="/signup">Sign up</Link>
+      {user && isAdmin ? (
+        <>
+          <Link to="/admin">Admin</Link>
+          <br />
+        </>
+      ) : null}
+      {!user ? (
+        <>
+          <Link to="/signin">Sign in</Link>
+          <br />
+          <Link to="/signup">Sign up</Link>
+        </>
+      ) : (
+        <>
+          <Link to="/signout">Sign out</Link>
+          <br />
+        </>
+      )}
     </div>
   );
 }
