2732a0be26b582e3e23996505b0bed75eef917e4
Asil <asilefeakyol@gmail.com>
2023-06-04 13:38:09 +0300

    handle bidding

--- app/routes/listings.$id.tsx
+++ app/routes/listings.$id.tsx
@@ -1,12 +1,26 @@
-import { Link, useLoaderData } from "@remix-run/react";
-import { getListing } from "~/server/listing.server";
+import { Form, Link, useActionData, useLoaderData } from "@remix-run/react";
+import { bidOnListing, getListing } from "~/server/listing.server";
 import type { IListing } from "~/server/listing.server";
-import type { LoaderArgs } from "@remix-run/node";
+import type { LoaderArgs, ActionArgs } from "@remix-run/node";
 import { clientDB } from "../utils/firebase";
 import { doc, onSnapshot } from "firebase/firestore";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 // import invariant from "tiny-invariant";
+
+export async function action({ request }: ActionArgs) {
+  const formData = await request.formData();
+
+  const id = formData.get("id");
+  const bid = formData.get("bid");
+
+  if (!id || !bid) return new Response("Missing id or bid", { status: 400 });
+
+  return await bidOnListing({
+    listingID: id.toString(),
+    bid: bid.toString(),
+  });
+}
 
 export let loader = async ({ params, request }: LoaderArgs) => {
   // invariant(params.slug, "expected params.slug");
@@ -20,13 +34,41 @@ export let loader = async ({ params, request }: LoaderArgs) => {
 
 export default function Listing() {
   let loaderListing = useLoaderData<IListing>();
+  const actionData = useActionData();
 
   const [listing, setListing] = useState(loaderListing);
+  const [isListingActive, setIsListingActive] = useState(false);
+  const [startsAt, setStartsAt] = useState(new Date(listing.startsAt));
+  const [endsAt, setEndsAt] = useState(new Date(listing.endsAt));
+  const [isListingExpired, setIsListingExpired] = useState(false);
+
+  const handleExpire = useCallback((docData: any) => {
+    const startsAt = new Date(docData.startsAt);
+    const endsAt = new Date(docData.endsAt);
+    if (startsAt.getTime() < Date.now() && endsAt.getTime() > Date.now()) {
+      setIsListingActive(true);
+      setIsListingExpired(false);
+    }
+
+    if (endsAt.getTime() < Date.now()) {
+      setIsListingExpired(true);
+      setIsListingActive(false);
+    }
+    setStartsAt(startsAt);
+    setEndsAt(endsAt);
+
+    if (listing.soldTo) {
+      setIsListingActive(false);
+      setIsListingExpired(true);
+    }
+
+  }, []);
 
   useEffect(() => {
     const unsub = onSnapshot(doc(clientDB, "listings", listing.id), (doc) => {
       console.log("Current data: ", doc.data());
       setListing({ ...listing, ...doc.data() });
+      handleExpire(doc.data());
     });
 
     return () => {
@@ -37,9 +79,49 @@ export default function Listing() {
   return (
     <div>
       <h2>{listing.title}</h2>
-      <p>{listing.id}</p>
+      <p>
+        ({listing.id} - {listing.slug})
+      </p>
       <p>{listing.description}</p>
-      <p>{listing.openingBid} TRY</p>
+      {isListingActive && (
+        <p style={{ color: "red", fontWeight: "bold" }}>
+          THIS LISTING IS CURRENTLY ACTIVE, BID NOW!
+        </p>
+      )}
+      {isListingExpired && (
+        <p style={{ color: "red", fontWeight: "bold" }}>
+          This listing has expired {":("}
+        </p>
+      )}
+      <p>
+        <strong>Auction starts at: </strong>
+        {JSON.stringify(startsAt)}
+      </p>
+      <p>
+        <strong>Auction ends at: </strong>
+        {JSON.stringify(endsAt)}
+      </p>
+      <p>
+        <strong>Opening bid: </strong>
+        {listing.openingBid} TRY
+      </p>
+      <p>
+        <strong>Increment bid:</strong> {listing.incrementBid} TRY
+      </p>
+      <p>
+        <strong>Current bid: </strong>
+        {listing.currentBid ? listing.currentBid + " TRY" : "N/A"}
+      </p>
+      <p>
+        <strong>Instant buy price: </strong>
+        {listing.instantBuyPrice} TRY
+      </p>
+      <Form method="post">
+        <input type="hidden" name="id" value={listing.id} />
+        <input type="number" name="bid" placeholder="Bid" />
+        <button type="submit">Bid</button>
+        {actionData && <p>{actionData}</p>}
+      </Form>
       <Link to="../listings">Go back</Link>
     </div>
   );
--- app/server/listing.server.ts
+++ app/server/listing.server.ts
@@ -1,19 +1,23 @@
 import { adminDB } from "~/server/firebaseAdmin.server";
+import { getUser } from "./auth.server";
+import { getUserAccount } from "./account.server";
+import { FieldValue } from "firebase-admin/firestore";
 
 export interface IListing {
   title: string;
   slug: string;
   description: string;
-  openingBid: number;
-  currentBid: number;
-  incrementBid: number;
-  instantBuyPrice: number;
+  openingBid: number | string;
+  currentBid: number | string;
+  incrementBid: number | string;
+  instantBuyPrice: number | string;
   media: any; // !! change this !! //
   startsAt: Date;
   endsAt: Date;
   soldTo: null | string;
   soldAt: null | Date;
   id: string;
+  bids: Object[];
 }
 
 interface IGetListing {
@@ -80,4 +84,84 @@ export async function listenToListing({ listingID }: any) {
   docRef.onSnapshot((doc: any) => {
     console.log("Current data: ", doc.data());
   });
+}
+
+export async function bidOnListing({ request, listingID, bid }: any) {
+  const docRef = adminDB.collection("listings").doc(listingID);
+
+  const doc = await docRef.get();
+
+  const user = await getUser();
+
+  if (!user) return new Response("You are not logged in.", { status: 401 });
+
+  const userAccount = await getUserAccount(request);
+  //@ts-ignore
+  if (!userAccount.address || !userAccount.phone)
+    return new Response(
+      "You need to enter your address and phone number in order to place a bid.",
+      { status: 401 }
+    );
+
+  if (!doc.exists) {
+    throw Error("No such document exists");
+  } else {
+    // @ts-ignore
+    const listing: IListing = doc.data();
+
+    if (listing.soldTo)
+      return new Response("Listing has already been sold", { status: 400 });
+    if (listing.endsAt < new Date())
+      return new Response("Listing has expired", { status: 400 });
+    if (listing.instantBuyPrice <= bid) {
+      await docRef.update({
+        currentBid: bid,
+        soldTo: user.uid,
+        soldAt: new Date(),
+        bids: FieldValue.arrayUnion({ uid: user.uid, bid }),
+      });
+      return new Response(
+        "Listing has been sold to you for instant buy price!",
+        { status: 200 }
+      );
+    }
+
+    if (!listing.currentBid && bid >= listing.openingBid) {
+      if (
+        bid > listing.openingBid &&
+        bid - parseInt(listing.openingBid) < parseInt(listing.incrementBid)
+      )
+        return new Response(
+          "Opening bid must be greater than increment bid or the same as opening bid.",
+          { status: 400 }
+        );
+      await docRef.update({
+        currentBid: bid,
+        bids: FieldValue.arrayUnion({ uid: user.uid, bid }),
+      });
+
+      return new Response(
+        "Bid has been placed and the auction has been started!",
+        { status: 200 }
+      );
+    }
+
+    // !! type check !!
+    if (listing.currentBid >= bid || listing.openingBid > bid) {
+      return new Response("Bid must be greater than current bid");
+    } else if (
+      parseInt(bid) - parseInt(listing.currentBid) <
+      parseInt(listing.incrementBid)
+    ) {
+      return new Response("Your bid must be greater than the increment bid.", {
+        status: 400,
+      });
+    } else {
+      await docRef.update({
+        currentBid: bid,
+        bids: FieldValue.arrayUnion({ uid: user.uid, bid }),
+      });
+      return new Response("Bid has been placed!", { status: 200 });
+    }
+  }
 }
